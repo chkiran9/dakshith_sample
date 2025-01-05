b902a2a729b27db2332961dbe7479aca7336c783
kirankumarchalike <chalikekiran1@gmail.com>
2025-01-05 19:31:49 +0000

    dakshith codinng py

---
+++ multiplication.py
@@ -0,0 +1,15 @@
+# write a program that multiplies two numbers together
+def multiply(x, y):
+    print("Hi i am in multiply function")
+    return x * y
+
+#print(multiply(4, 5))
+
+# write a program that adds two numbers together
+def add(x, y):
+    print(("Hi i am in add function"))
+    return x + y
+
+print(add(2, 5))
+print(add(72, 27))
+print(multiply(6 ,4))
