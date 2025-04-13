9447b30b2595bd1008c6ea5b0cff686b2d6584c0
kirankumarchalike <chalikekiran1@gmail.com>
2025-04-13 16:07:48 +0100

    dakshith multiplication code

---
+++ muliplication_table.py
@@ -0,0 +1,16 @@
+# write multiplication table
+def multiplication(i = 1):
+     print("Multiplication Table")
+     for j in range(1, 11):
+            print(f"{i} x {j} = {i * j}")
+
+multiplication(i=9)
+
+
+
+
+
+
+
+
+
