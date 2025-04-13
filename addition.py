66aec189ea3b09bc1d339f98608e13b143181bd5
kirankumarchalike <chalikekiran1@gmail.com>
2025-04-13 16:25:22 +0100

    dakshith addition code

---
+++ addition.py
@@ -0,0 +1,6 @@
+#add seven numbers together
+def add_seven_numbers(a, b, c, d, e, f, g):
+  print("Hi i am in add_seven_numbers function")
+  return a + b + c + d + e + f + g
+
+print(add_seven_numbers(100, 200, 300, 400, 500, 600, 700))
